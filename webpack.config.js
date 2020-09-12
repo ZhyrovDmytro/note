e6b362eb2e2287631efa13d296a9f0da9501b8d1
Dmytro Zhyrov <Dmitriy_zhirov@mail.ru>
2020-09-12 22:30:39 +0200

    Gitignore (#2)
    * gitignore
    
    * setup
    
    * Delete package-lock.json
    
    Co-authored-by: dmytro.zhyrov <dmytro.zhyrov@sinnerschrader.com>

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+const APP_DIR = path.resolve(__dirname, './client');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: APP_DIR + '/src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, BUILD_DIR)
+    },
+    module: {
+        rules: [
+            {
+                test   : /\.js|.jsx|.tsx|.ts$/,
+                exclude: /node-modules/,
+                loader : "babel-loader",
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js', '.ts' ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './public/index.html', filename: "./index.html",})
+    ]
+};
