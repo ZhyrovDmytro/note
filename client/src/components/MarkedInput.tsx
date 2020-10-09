1cdc49753a2574ff911763a62f9f8c6e96b882a6
dmytro.zhyrov <dmytro.zhyrov@sinnerschrader.com>
2020-10-09 17:16:36 +0200

    richtext setup: bold,italic, underline

--- client/src/components/MarkedInput.tsx
+++ client/src/components/MarkedInput.tsx
@@ -1,6 +1,12 @@
+import {Attributes, KeyboardEventHandler} from 'react';
 import * as React from 'react';
-import { createEditor, Node, Transforms, Editor } from 'slate'
+import { createEditor, Node, Transforms, Editor, Text } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
+import Icon from 'react-icons-kit';
+import {bold} from 'react-icons-kit/feather/bold'
+import {code} from 'react-icons-kit/feather/code'
+import {underline} from 'react-icons-kit/feather/underline'
+import {italic} from 'react-icons-kit/feather/italic'
 
 const CodeElement = (props: any) => {
     return (
@@ -10,8 +16,95 @@ const CodeElement = (props: any) => {
     )
 };
 
+interface LeafProps {
+    children: Node[];
+    attributes: Attributes;
+    leaf: any;
+}
+
+const CustomEditor = {
+    isBoldMarkActive(editor: Editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.bold === true,
+            universal: true,
+        });
+
+        return !!match
+    },
+    isItalicMarkActive(editor: Editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.italic === true,
+            universal: true,
+        });
+
+        return !!match
+    },
+    isUnderlinedMarkActive(editor: Editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.underline === true,
+            universal: true,
+        });
+
+        return !!match
+    },
+    isCodeBlockActive(editor: Editor) {
+        const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'code',
+        });
+
+        return !!match
+    },
+    toggleBoldMark(editor: Editor) {
+        const isActive = CustomEditor.isBoldMarkActive(editor);
+        Transforms.setNodes(
+            editor,
+            { bold: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    toggleItalicMark(editor: Editor) {
+        const isActive = CustomEditor.isItalicMarkActive(editor);
+        Transforms.setNodes(
+            editor,
+            { italic: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    toggleUnderlineMark(editor: Editor) {
+        const isActive = CustomEditor.isUnderlinedMarkActive(editor);
+        Transforms.setNodes(
+            editor,
+            { underline: isActive ? null : true },
+            { match: n => Text.isText(n), split: true }
+        )
+    },
+    toggleCodeBlock(editor: Editor) {
+        const isActive = CustomEditor.isCodeBlockActive(editor);
+        Transforms.setNodes(
+            editor,
+            { type: isActive ? null : 'code' },
+            { match: n => Editor.isBlock(editor, n) }
+        )
+    },
+};
+
+
 const DefaultElement = (props: any) => {
     return <p {...props.attributes}>{props.children}</p>
+};
+
+const Leaf = (props: LeafProps) => {
+    return (
+        <span
+            {...props.attributes}
+            style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal',
+                     fontStyle: props.leaf.italic ? 'italic': 'unset',
+                     textDecoration: props.leaf.underline ? 'underline': 'unset'
+            }}
+        >
+      {props.children}
+    </span>
+    )
 };
 
 export function MarkedInput(): JSX.Element {
@@ -32,23 +125,79 @@ export function MarkedInput(): JSX.Element {
         }
     }, []);
 
+    const renderLeaf = React.useCallback(props => {
+        return <Leaf {...props} />
+    }, []);
+
+    const handleKeyPress = (e: KeyboardEvent): void => {
+        if (!e.ctrlKey) {
+            return
+        }
+
+        switch (e.key) {
+            case '`': {
+                e.preventDefault();
+                CustomEditor.toggleCodeBlock(editor);
+                break
+            }
+
+            case 'b': {
+                e.preventDefault();
+               CustomEditor.toggleBoldMark(editor);
+                break
+            }
+
+            case 'i': {
+                e.preventDefault();
+                CustomEditor.toggleItalicMark(editor);
+                break
+            }
+
+            case 'u': {
+                e.preventDefault();
+                CustomEditor.toggleUnderlineMark(editor);
+                break
+            }
+        }
+    };
+
     return (
         <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
-            <Editable renderElement={renderElement} onKeyDown={event => {
-                if (event.key === '`' && event.ctrlKey) {
-                    event.preventDefault();
-                    // Determine whether any of the currently selected blocks are code blocks.
-                    const [match] = Editor.nodes(editor, {
-                        match: n => n.type === 'code',
-                    });
-                    // Toggle the block type depending on whether there's already a match.
-                    Transforms.setNodes(
-                        editor,
-                        { type: match ? 'paragraph' : 'code' },
-                        { match: n => Editor.isBlock(editor, n) }
-                    )
-                }
-            }}/>
+            <div>
+                <button
+                    onMouseDown={event => {
+                        event.preventDefault();
+                        CustomEditor.toggleBoldMark(editor)
+                    }}
+                >
+                    <Icon icon={bold} />
+                </button>
+                <button
+                    onMouseDown={event => {
+                        event.preventDefault();
+                        CustomEditor.toggleItalicMark(editor)
+                    }}
+                >
+                    <Icon icon={italic} />
+                </button>
+                <button
+                    onMouseDown={event => {
+                        event.preventDefault();
+                        CustomEditor.toggleUnderlineMark(editor)
+                    }}
+                >
+                    <Icon icon={underline} />
+                </button>
+                <button
+                    onMouseDown={event => {
+                        event.preventDefault();
+                        CustomEditor.toggleCodeBlock(editor)
+                    }}
+                >
+                    <Icon icon={code} />
+                </button>
+            </div>
+            <Editable renderElement={renderElement} renderLeaf={renderLeaf} onKeyDown={(event: KeyboardEvent) => handleKeyPress(event)}/>
         </Slate>
     )
 }
